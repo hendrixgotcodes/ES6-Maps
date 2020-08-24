6a340be6e46181c0c57fd4c94f48635a458da8e1
hendrixgotcodes <asare11samuel@gmail.com>
2020-08-25 01:55:23 +0200

    Added Comments

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+//Instantiiating a map object
 let newMap = new Map();
 
+//Adding keyvalue pairs
 newMap.set(0,"First Index");
 newMap.set(1,"Second Index");
 newMap.set(2,"Third Index");
 
+//Iterating through the map with a for..of loop
 for(let [key,value] of newMap){
-    console.log(`${key} ${value}`)
+    console.log(`Key: ${key}  Value: ${value}`)
 }
